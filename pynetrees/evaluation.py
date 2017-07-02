005b69c3e798d9ab20828d11ca27367b4e5723ee
KDercksen <mail@koendercksen.com>
2017-07-02 12:48:22 +0200

    Add evaluation module

---
+++ pynetrees/evaluation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def train_test_split(samples, test_size=0.3, seed=42):
+    np.random.seed(seed)
+    np.random.shuffle(samples)
+
+    split = int(samples.shape[0] * test_size)
+    return samples[:split], samples[split:]
